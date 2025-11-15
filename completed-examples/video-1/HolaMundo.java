65cba72621c1e3456e63d3237d12fc8e8abeab88
Trevor <valentinosolischang@gmail.com>
2025-11-14 21:55:56 -0500

    Add HolaMundo.java starter file with exercise instructions

---
+++ completed-examples/video-1/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * Lección 1: ¿Qué es Java y la Programación?
+ * 
+ * EJERCICIO RESUELTO: Programa básico que imprime "Hola Mundo" en la consola
+ * 
+ * CONCEPTOS:
+ * - Todo programa Java necesita una clase
+ * - El método main es el punto de entrada del programa
+ * - System.out.println() imprime texto en la consola
+ */
+
+public class HolaMundo {
+  public static void main(String[] args) {
+    // Imprimimos el mensaje "Hola Mundo"
+    System.out.println("Hola Mundo");
+
+    // También podemos imprimir otros mensajes
+    System.out.println("¡Bienvenido a Java!");
+  }
+}
---
+++ starter-files/video-1/HolaMundo.java
@@ -0,0 +1,16 @@
+/**
+ * Lección 1: ¿Qué es Java y la Programación?
+ * 
+ * EJERCICIO: Completa este programa para que imprima "Hola Mundo" en la consola
+ * 
+ * INSTRUCCIONES:
+ * 1. Completa el método main
+ * 2. Usa System.out.println() para imprimir el mensaje
+ */
+
+public class HolaMundo {
+  public static void main(String[] args) {
+    // TODO: Escribe tu código aquí para imprimir "Hola Mundo"
+
+  }
+}
